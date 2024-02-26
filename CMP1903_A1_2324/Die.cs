7b3c533f3dd56877c2e0a25a449da25132a5be54
Connor Adams <60903197+Smarterr@users.noreply.github.com>
2024-02-26 11:12:59 +0000

    Die Class Update
    Die Class is completed and working.

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -13,11 +13,17 @@ namespace CMP1903_A1_2324
          * and one method that rolls the die, returns and integer and takes no parameters.
          */
 
-        //Property
+        //Property to hold die value
+        public int DieValue { get; private set; }
 
 
-        //Method
-
+        //Method to roll the die and return result
+        public int Roll()
+        {
+            Random random = new Random();
+            DieValue = random.Next(1, 7); // Genrates a random number between 1 and 6
+            return DieValue;
+        }
 
     }
 }
