8f43b9eaf95952c61cacc8823f85e5f043af87ec
Connor Adams <60903197+Smarterr@users.noreply.github.com>
2024-02-26 11:44:14 +0000

    Rolling 3 Dice
    Completed the ability to roll 3 dice in game file + fixed die file so that all 3 dice didn't roll the same number.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -16,6 +16,32 @@ namespace CMP1903_A1_2324
          */
 
         //Methods
+        public static void StartGame()
+        {
 
+            // Create a single instance of Random to share among all dice
+            Random random = new Random();
+
+            // Create three instances of the Die class
+            Die die1 = new Die(random);
+            Die die2 = new Die(random);
+            Die die3 = new Die(random);
+
+            // Roll all three dice
+            int result1 = die1.Roll();
+            int result2 = die2.Roll();
+            int result3 = die3.Roll();
+
+            // Calculate the sum of the face values
+            int sum = result1 + result2 + result3;
+
+            // Output the results
+            Console.WriteLine("Results of rolling three dice:");
+            Console.WriteLine("Die 1: " + result1);
+            Console.WriteLine("Die 2: " + result2);
+            Console.WriteLine("Die 3: " + result3);
+            Console.WriteLine("Sum of face values: " + sum);
+            Console.ReadLine();
+        }
     }
 }
