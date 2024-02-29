ec169544a98f17935f1c6f2307af43c5c0afe9be
Connor Adams <60903197+Smarterr@users.noreply.github.com>
2024-02-29 14:29:40 +0000

    Remove from master

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -15,35 +15,7 @@ namespace CMP1903_A1_2324
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
 
-        public int SumOfDieValues { get; private set; }
-
-        //Methods
-        public void StartGame()
-        {
-            
-            // Create a single instance of Random to share among all dice
-            Random random = new Random();
-
-            // Create three instances of the Die class
-            Die die1 = new Die(random);
-            Die die2 = new Die(random);
-            Die die3 = new Die(random);
-
-            // Roll all three dice
-            int result1 = die1.Roll();
-            int result2 = die2.Roll();
-            int result3 = die3.Roll();
-
-            // Calculate the sum of the face values
-            SumOfDieValues = result1 + result2 + result3;
-
-            // Output the results
-            Console.WriteLine("Results of rolling three dice:");
-            Console.WriteLine("Die 1: " + result1);
-            Console.WriteLine("Die 2: " + result2);
-            Console.WriteLine("Die 3: " + result3);
-            Console.WriteLine("Sum of dice values: " + SumOfDieValues);
-            Console.ReadLine();
+     
         }
     }
 }
--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -19,29 +19,7 @@ namespace CMP1903_A1_2324
 
         //Method
 
-        public static void TestGame()
-        {
-            // Create a Game object
-            Game game = new Game();
-
-            game.StartGame();
-
-            int sum = game.SumOfDieValues;
-
-            // Assert that the sum is within the expected range based on the number of dice rolled (3 to 18 for three dice)
-            Debug.Assert(sum >= 3 && sum <= 18, $"Sum of die values ({sum}) out of range 3 to 18");
-        }
-
-        public static void TestDie()
-        {
-            // Create a Die object
-            Die die = new Die(new Random());
-
-            // Tests that the die roll is inbetween 1 and 6
-            int rollResult = die.Roll();
-            Debug.Assert(rollResult >= 1 && rollResult <= 6, $"Die roll result ({rollResult}) out of range.");
-
-        }
+        
     }
 }
 
